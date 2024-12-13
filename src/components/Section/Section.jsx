1bce56416962a2abcc8ee86935e88bdada03d06c
AsilDEV001 <142443592+AsilDEV001@users.noreply.github.com>
2024-12-14 00:34:29 +0500

    adapting for mobile

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -30,64 +30,25 @@ import Notification from './notification.png'
   
     return (
       <div className="section-container">
-        <div className="search-bar">
           <input
             type="text"
+            className='search-bar'
             placeholder="Qidirish..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           />
-        </div>
         {filteredInformation.map((info) => (
           <CardContainer key={info.id} info={info} />
         ))}
+
+        <div className='support'>
+           <img src={Support} alt='support'/>
+         </div>
+         <div className="notification">
+           <img src={Notification} alt="notification" />
+         </div>
       </div>
     );
   };
-  
-  // const [information, setInfo] = useState([]);
-  // const [searchQuery, setSearchQuery] = useState('');
-  // const colRef = collection(db, 'wanted');
-  
-  // useEffect(() => {
-  //     const getInformation = async() => {
-  //         const data = await getDocs(colRef)
-  //         setInfo(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
-  //       }
-  //       getInformation()
-  //     }, []) 
-    
-  //     const filteredInformation = information.filter((info) =>
-  //         Object.values(info)
-  //         .join(' ')
-  //         .toLowerCase()
-  //         .includes(searchQuery.toLowerCase())
-  //     );
-    
-  //     return (
-  //         <div className="section-container">
-      
-  //           <div className="search-bar">
-  //             <input
-  //         type="text"
-  //         placeholder="Qidirish..."
-  //         value={searchQuery}
-  //         onChange={(e) => setSearchQuery(e.target.value)}
-  //       />
-  //     </div>
-
-  //     {filteredInformation.map((info) => (
-  //         <CardContainer key={info.id} info={info} />
-  //       ))}
-  
-  //       <div className='support'>
-  //         <img src={Support} alt='support'/>
-  //       </div>
-  //       <div className="notification">
-  //         <img src={Notification} alt="notification" />
-  //       </div>
-  //     </div>
-  //   )
-// }
   
 export default SectionContainer;    
